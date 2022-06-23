af0beac82b9f17045614b420041f861108554f23
matsumoto-minami-26 <matsumoto@p-partners.co.jp>
2022-06-24 01:55:08 +0900

    課題18の追加

--- Gemfile
+++ Gemfile
@@ -43,6 +43,8 @@ gem 'rails-i18n', '5.1.3'
 gem 'draper'
 gem 'carrierwave', '~> 2.0'
 gem "mini_magick"
+gem 'kaminari'
+gem 'bootstrap4-kaminari-views'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,7 +2,7 @@ class BoardsController < ApplicationController
   before_action :set_board, only: %i[edit update destroy]
 
   def index
-    @boards = Board.all.includes(:user).order(created_at: :desc)
+    @boards = Board.all.includes(:user).order(created_at: :desc).page(params[:page])
   end
   
   def show
@@ -47,7 +47,7 @@ class BoardsController < ApplicationController
 
   def bookmarks
     # binding.pry
-    @bookmark_boards = current_user.bookmark_boards.includes(:user).order(created_at: :desc)
+    @bookmark_boards = current_user.bookmark_boards.includes(:user).order(created_at: :desc).page(params[:page])
   end
   
   private
